b3fad313ec843209c900b837f924f28867dbb3c6
daisuke_tanabe <helloworld0840@gmail.com>
2019-03-14 12:07:25 +0900

    feat: add project id

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 #!/usr/bin/env node
 const yargs = require('yargs');
 const pkg = require('./package.json');
+const config = require('./holmes.config.json');
+
+const getBranches = () => {
+  console.log(config.gitlab.projectsId);
+};
+
+const branch = {
+  command: 'branch',
+  aliases: ['b'],
+  desc: 'チームが抱えているGitlabのリポジトリからトピックブランチを取得',
+  handler: argv => {
+    getBranches();
+  }
+};
 
 yargs
+  .command(branch)
   .version('version', 'バージョン情報の表示', `v${pkg.version}`).alias('v', 'version')
   .help('help', 'ヘルプの表示').alias('h', 'help')
   .argv;
