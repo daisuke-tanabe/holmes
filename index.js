96085903828dbb826872cd07bc5feec8a87a9203
daisuke_tanabe <helloworld0840@gmail.com>
2019-03-14 12:48:57 +0900

    feat: get branches data

--- index.js
+++ index.js
@@ -2,9 +2,13 @@
 const yargs = require('yargs');
 const pkg = require('./package.json');
 const config = require('./holmes.config.json');
+const { execSync } = require('child_process');
 
 const getBranches = () => {
-  console.log(config.gitlab.projectsId);
+  for (const id of config.gitlab.projectsId) {
+    const data = execSync(`curl https://${config.gitlab.domain}/api/v4/projects/${id}/repository/branches?private_token=${config.gitlab.token}`).toString();
+    console.log(data);
+  }
 };
 
 const branch = {
